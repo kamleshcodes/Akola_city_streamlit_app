b9ccefd2fa1e39b282c5b1e24cf82e727fcdb66a
Kamlesh_Sawadekar <sawadekarkamlesh@gmail.com>
2020-06-24 02:16:59 +0530

    update5

--- akola.py
+++ akola.py
@@ -23,7 +23,7 @@ def load_data(nrows):
     return data
 
 
-data=load_data(988)
+data=load_data(989)
 data[['latitude','longitude']].to_csv('lat_long.csv', index=False)
 
 st.header("Where have been Covid-19 patients detected in Akola city?")
@@ -35,10 +35,10 @@ if choose1:
     data1.reset_index(drop=True, inplace=True)
 else:
     st.markdown("Use the slider to see the spread Covid-19 in the past.")
-    time=st.slider("Travel back in time =))", 0, 37)
+    time=st.slider("Travel back in time =))", 0, 38)
     date1 = data.date[0]
     date2= date1 + datetime.timedelta(days=time)
-    date_disp = date1 + datetime.timedelta(days=(37-time))
+    date_disp = date1 + datetime.timedelta(days=(38-time))
     st.write(date_disp)
     data1= data.loc[data['date']>=date2]
     data1.reset_index(drop=True, inplace=True)
